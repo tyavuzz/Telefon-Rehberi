026235beecde0207f0f9061425d6fd6bdaecbf42
Tamer Yavuz <50012950+tyavuzz@users.noreply.github.com>
2019-05-03 18:44:16 +0300

    Add files via upload

---
+++ TEl Rehber/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace TEl_Rehber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.telRehberDataSet = new TEl_Rehber.TelRehberDataSet();
+            this.tblKisilerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tbl_KisilerTableAdapter = new TEl_Rehber.TelRehberDataSetTableAdapters.Tbl_KisilerTableAdapter();
+            this.telRehberDataSet1 = new TEl_Rehber.TelRehberDataSet();
+            this.tblKisilerBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.telRehberDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telRehberDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource1)).BeginInit();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.richTextBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.groupBox1.Size = new System.Drawing.Size(375, 499);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Yeni ";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(113, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(235, 29);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(113, 89);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(235, 29);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(113, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(235, 29);
+            this.textBox3.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(113, 186);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(235, 29);
+            this.textBox4.TabIndex = 3;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(113, 236);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(235, 29);
+            this.textBox5.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Adı :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Soyadı: ";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(82, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Cep tel:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 191);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "İş tel:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 24);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "E-mail ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 286);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(71, 24);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Adres:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(113, 286);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(235, 96);
+            this.richTextBox1.TabIndex = 11;
+            this.richTextBox1.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 404);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(235, 35);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Kaydet";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 25);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(896, 382);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.dataGridView1);
+            this.groupBox2.Location = new System.Drawing.Point(384, 89);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(902, 410);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // telRehberDataSet
+            // 
+            this.telRehberDataSet.DataSetName = "TelRehberDataSet";
+            this.telRehberDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblKisilerBindingSource
+            // 
+            this.tblKisilerBindingSource.DataMember = "Tbl_Kisiler";
+            this.tblKisilerBindingSource.DataSource = this.telRehberDataSet;
+            // 
+            // tbl_KisilerTableAdapter
+            // 
+            this.tbl_KisilerTableAdapter.ClearBeforeFill = true;
+            // 
+            // telRehberDataSet1
+            // 
+            this.telRehberDataSet1.DataSetName = "TelRehberDataSet";
+            this.telRehberDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tblKisilerBindingSource1
+            // 
+            this.tblKisilerBindingSource1.DataMember = "Tbl_Kisiler";
+            this.tblKisilerBindingSource1.DataSource = this.telRehberDataSet1;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button4);
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(384, 13);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(902, 70);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(26, 25);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 33);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Listele";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(197, 23);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 33);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "Düzenle";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(380, 19);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(143, 33);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Sil";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1298, 526);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.telRehberDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.telRehberDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tblKisilerBindingSource1)).EndInit();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private TelRehberDataSet telRehberDataSet;
+        private System.Windows.Forms.BindingSource tblKisilerBindingSource;
+        private TelRehberDataSetTableAdapters.Tbl_KisilerTableAdapter tbl_KisilerTableAdapter;
+        private TelRehberDataSet telRehberDataSet1;
+        private System.Windows.Forms.BindingSource tblKisilerBindingSource1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ TEl Rehber/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TEl_Rehber
+{
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        static string conString = "Server=.\\sqlexpress;Database=TelRehber;User Id=sa;Password=saw;";
+       
+        SqlConnection baglanti = new SqlConnection(conString);
+        DataSet ds = new DataSet();
+
+
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (baglanti.State == ConnectionState.Closed)
+                    baglanti.Open();
+          
+                string kayit = "INSERT INTO Tbl_Kisiler (AD, SOYAD, CEP_TEL, IS_TEL, EMAIL, ADRES) VALUES ( @AD,@SOYAD,@CEPTEL,@ISTEL,@EMAIL,@ADRES)";
+            
+                SqlCommand komut = new SqlCommand(kayit, baglanti);
+              
+                komut.Parameters.AddWithValue("@AD", textBox1.Text);
+                komut.Parameters.AddWithValue("@SOYAD", textBox2.Text);
+                komut.Parameters.AddWithValue("@CEPTEL", textBox3.Text);
+                komut.Parameters.AddWithValue("@ISTEL", textBox4.Text);
+                komut.Parameters.AddWithValue("@EMAIL", textBox5.Text);
+                komut.Parameters.AddWithValue("@ADRES", richTextBox1.Text);
+                //Parametrelerimize Form üzerinde ki kontrollerden girilen verileri aktarıyoruz.
+                komut.ExecuteNonQuery();
+                //Veritabanında değişiklik yapacak komut işlemi bu satırda gerçekleşiyor.
+         
+                MessageBox.Show(" Kayıt İşlemi Gerçekleşti.","Bilgi", MessageBoxButtons.OK,MessageBoxIcon.Information);
+                //
+                baglanti.Close();
+            }
+            catch (Exception hata)
+            {
+                baglanti.Close();
+                MessageBox.Show("İşlem Sırasında Hata Oluştu, bilgiler kaydedilemedi."+hata ,"dikkat",MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'telRehberDataSet1.Tbl_Kisiler' table. You can move, or remove it, as needed.
+            this.tbl_KisilerTableAdapter.Fill(this.telRehberDataSet1.Tbl_Kisiler);
+            // TODO: This line of code loads data into the 'telRehberDataSet.Tbl_Kisiler' table. You can move, or remove it, as needed.
+            this.tbl_KisilerTableAdapter.Fill(this.telRehberDataSet.Tbl_Kisiler);
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            if (ds.Tables.Count > 0)
+                dataGridView1.Columns.Clear();
+            ds.Tables.Clear();
+            dataGridView1.Refresh();
+
+
+
+
+
+
+            baglanti.Open();
+            try
+            {
+                dataGridView1.DataSource = null; //Her click de datasource u null a eşitleyip içeriğini temizliyoruz
+
+
+                string Sqlcmd = "SELECT AD 'ADI', SOYAD 'SOYADI', CEP_TEL 'CEP TELEFONU', IS_TEL 'İŞ TELEFONU', EMAIL 'MAİL ADRESİ', ADRES 'ADRES'  FROM Tbl_Kisiler";
+                SqlDataAdapter da = new SqlDataAdapter(Sqlcmd, baglanti);//dataapter nesnesini oluşturup sqlCmd sorgu cümlesini ve sqlCon veritabanı bağlantımızı yazıyoruz
+                //dataset nesnesini oluşturuyoruz
+                da.Fill(ds, "Tbl_Kisiler");//sqlCmd sorgusundan gelen veriyi dataset nesnesine ekliyoruz. ben burada table ismi için Person dedim siz başka bir isimde verebilirsiniz
+
+                if (ds.Tables[0].Rows.Count == 0)//Person tablosunda herhangi bir veri yoksa (boşsa) aşağıdaki blok çalışacak
+                {
+                   
+                    return;//kayıt olmadığı için return ile bloğun dışına çıkıyoruz
+                }
+                else//kayıt varsa
+                {
+                    dataGridView1.AutoResizeColumns();
+                    dataGridView1.AutoSizeColumnsMode =         DataGridViewAutoSizeColumnsMode.AllCells;
+                    dataGridView1.DataSource = ds.Tables["Tbl_Kisiler"];//sqlCmd sorgusu ile çektiğimiz kayıtlar datagridview1 üzerinde gösteriliyor
+                }
+
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show("Hata : " + ex); //Veritabanına bağlantı sırasında alınan bir hata varsa burada gösteriliyor
+
+            }
+            finally //button1_Click olduğu sürece bu bloğa uğramadan uygulama sonlanmıyor
+            {
+                baglanti.Close(); //Açık olan Sql bağlantısı sonlandırılıyor
+
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            int SecilenSatir;
+            SecilenSatir = dataGridView1.CurrentRow.Index;
+
+            textBox1.Text = dataGridView1.Rows[SecilenSatir].Cells[0].Value.ToString();
+            textBox2.Text = dataGridView1.Rows[SecilenSatir].Cells[1].Value.ToString();
+            textBox3.Text = dataGridView1.Rows[SecilenSatir].Cells[2].Value.ToString();
+            textBox4.Text = dataGridView1.Rows[SecilenSatir].Cells[3].Value.ToString();
+            textBox5.Text = dataGridView1.Rows[SecilenSatir].Cells[4].Value.ToString();
+            richTextBox1.Text = dataGridView1.Rows[SecilenSatir].Cells[5].Value.ToString();
+        }
+       
+
+        private void dataGridView1_RowEnter(object sender, DataGridViewCellEventArgs e)
+        {
+            
+
+
+      
+           
+         
+        }
+    }
+}
